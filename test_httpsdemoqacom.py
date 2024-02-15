a2aedcc8dc0b2c1b83241f28b55a32a6868d2a03
KininiQA <124257394+KininiQA@users.noreply.github.com>
2024-02-15 15:28:01 +0200

    Удалена ненужная строчка кода.
    from selenium.webdriver.common.action_chains import ActionChains

--- test_httpsdemoqacom.py
+++ test_httpsdemoqacom.py
@@ -4,7 +4,6 @@ import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
